daed8dd8dd88f5a179668ef4e17503b418b1e3f3
ABDUL MATIN MUSYARIF MUHAMMAT <abdul_25004266@utp.edu.my>
2026-07-06 22:04:16 +0800

    test 3

--- OMC_PROJECT/formbalance.cs
+++ OMC_PROJECT/formbalance.cs
@@ -16,5 +16,10 @@ namespace OMC_PROJECT
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hi");
+        }
     }
 }
