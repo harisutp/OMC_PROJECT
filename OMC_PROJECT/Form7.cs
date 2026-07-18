ff004ba909e99c510316572088a32bb56296a784
qqyuz <damia173@gmail.com>
2026-07-19 00:49:29 +0800

    background signup

---
+++ OMC_PROJECT/Form7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OMC_PROJECT
+{
+    public partial class formHome : Form
+    {
+  
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
